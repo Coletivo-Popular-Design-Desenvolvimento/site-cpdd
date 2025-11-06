8b393a34ca3c9aea957e6ecc879ec74d918d88c0
Danilo Nobre Nunes <danilonobren@gmail.com>
2025-11-06 19:08:12 -0300

    usando o packages/cpdd-ui

--- apps/cpdd-site/src/app/layout.tsx
+++ apps/cpdd-site/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import PrelineScriptWrapper from '@components/Preline/PrelineScriptWrapper';
+
+import { PrelineScriptWrapper } from '@cpdd/ui';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
