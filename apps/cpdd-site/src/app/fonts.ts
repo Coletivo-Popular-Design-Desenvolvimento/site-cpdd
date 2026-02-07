fc3713feecca88398c864cb9cb10037cbbcb89e2
rev-oo <115083816+rev-oo@users.noreply.github.com>
2026-02-07 14:10:02 -0300

    feat: change global font to Barlow Condensed

---
+++ apps/cpdd-site/src/app/fonts.ts
@@ -0,0 +1,7 @@
+import { Barlow_Condensed } from "next/font/google";
+
+export const barlowCondensed = Barlow_Condensed({
+  variable: "--font-barlow-condensed",
+  weight: ["400", "500", "600"],
+  subsets: ["latin"],
+});
