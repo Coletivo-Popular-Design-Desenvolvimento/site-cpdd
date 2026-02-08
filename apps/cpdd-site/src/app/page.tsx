12a1efa8c1b23341419ae048148e7a8bd22d984b
rev-oo <115083816+rev-oo@users.noreply.github.com>
2026-02-08 20:27:28 -0300

    desktop font size and layout

--- apps/cpdd-site/src/app/page.tsx
+++ apps/cpdd-site/src/app/page.tsx
@@ -3,11 +3,13 @@ import Footer from "./ui/Footer";
 
 export default function Home() {
     return (
-        <main className='max-w-5xl mx-auto p-4 space-y-8'>
-            <h1 className="text-4xl font-bold">Site do CPDD</h1>
-            <FAQ />
-            <h2>EXEMPLO MANERÃO</h2>
+        <>
+            <main className='max-w-5xl mx-auto p-4 space-y-8'>
+                <h1 className="text-4xl font-bold">Site do CPDD</h1>
+                <FAQ />
+                <h2>EXEMPLO MANERÃO</h2>
+            </main>
             <Footer/>
-        </main>
+        </>
     );
 }
--- apps/cpdd-site/src/app/ui/Footer.tsx
+++ apps/cpdd-site/src/app/ui/Footer.tsx
@@ -5,21 +5,24 @@ const emailAddress = "coletivo@cpdd.com.br";
 export default function Footer () {
   return (
     <footer className="flex flex-col py-8 md:py-0 bg-white md:bg-[#F34822] md:flex-col-reverse">
-      <div className="bg-white text-black text-sm pb-3 md:py-18">
-        <p className="font-semibold mb-1">Tecnologia nas mãos de quem trabalha</p>
-        <p className="text-xs">Quer colaborar, sugerir ideias ou fazer parte dos nossos projetos?</p>
-        <p className="mb-3 text-xs">Mande uma mensagem! Estamos sempre abertos a novas conexões e oportunidades.</p>
-        <p className="mb-3 text-xs">&copy; 2024. CPDD - Coletivo Popular de Design e Desenvolvimento. Todos os direitos reservados.</p>
-        <p>
-          <span className="font-semibold">E-mail para contato e parcerias:</span>
-          <br />
-          <a href={`mailto:${emailAddress}`}>{emailAddress}</a>
-        </p>
+      <div className="bg-white text-black text-sm pb-3 md:py-18 md:flex md:justify-between md:items-center md:gap-4">
+        <div>
+          <p className="font-semibold mb-1 md:text-3xl">Tecnologia nas mãos de quem trabalha</p>
+          <p className="text-xs md:text-xl">Quer colaborar, sugerir ideias ou fazer parte dos nossos projetos?</p>
+          <p className="mb-3 text-xs md:text-xl">Mande uma mensagem! Estamos sempre abertos a novas conexões e oportunidades.</p>
+          <p className="mb-3 text-xs md:text-xl">&copy; 2024. CPDD - Coletivo Popular de Design e Desenvolvimento. Todos os direitos reservados.</p>
+        </div>
+        <div>
+          <p className="md:text-2xl">
+            <span className="font-semibold">E-mail para contato e parcerias:</span>
+            <br />
+            <a href={`mailto:${emailAddress}`}>{emailAddress}</a>
+          </p>
+        </div>
       </div>
-      <div className="bg-white self-end md:self-start md:py-8 md:pr-40 md:relative md:after:h-full md:after:left-full md:after:top-0 md:after:bg-inherit md:after:absolute md:after:clip-wave">
+      <div className="bg-white self-end md:self-start md:py-8 md:pr-40 md:relative md:after:h-full md:after:left-full md:after:top-0 md:after:bg-inherit md:after:absolute md:after:clip-wave-right">
         <CpddLogo className="w-40 md:w-80" width={320} height={64} />
       </div>
     </footer>
   );
 }
-
