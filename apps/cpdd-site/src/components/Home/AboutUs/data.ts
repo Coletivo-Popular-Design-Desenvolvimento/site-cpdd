80087ba8dc30c641122510e76cbe7ffc47d1e708
Danilo Nobre Nunes <danilonobren@gmail.com>
2026-02-09 21:39:26 -0300

    feat: data e types

---
+++ apps/cpdd-site/src/components/Home/AboutUs/data.ts
@@ -0,0 +1,37 @@
+import type { Panel } from "./types"
+
+export const panels: Panel[] = [
+    {
+        id: 0,
+        label: "Lorem Ipsum 01",
+        color: "#00BCD4",
+        dotColor: "#00BCD4",
+        title: "Objetivos",
+        description: "foo",
+        activitiesTitle: "Atividades Principais",
+        activities: ["bar", "foobar"],
+        image: "https://picsum.photos/seed/panel0/800/600",
+    },
+    {
+        id: 1,
+        label: "Lorem Ipsum 02",
+        color: "#00BCD4",
+        dotColor: "#00BCD4",
+        title: "Objetivos",
+        description: "foo",
+        activitiesTitle: "Atividades Principais",
+        activities: ["bar", "foobar"],
+        image: "https://picsum.photos/seed/panel1/800/600",
+    },
+    {
+        id: 2,
+        label: "Lorem Ipsum 03",
+        color: "#00BCD4",
+        dotColor: "#00BCD4",
+        title: "Objetivos",
+        description: "foo",
+        activitiesTitle: "Atividades Principais",
+        activities: ["bar", "foobar"],
+        image: "https://picsum.photos/seed/panel2/800/600",
+    },
+]
---
+++ apps/cpdd-site/src/components/Home/AboutUs/types.ts
@@ -0,0 +1,11 @@
+export interface Panel {
+    id: number
+    label: string
+    color: string
+    dotColor: string
+    title: string
+    description: string
+    activitiesTitle: string
+    activities: string[]
+    image: string
+}
