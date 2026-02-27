630661a0888427de02b72728ac3fdd76129c9787
José Victor Dias Rodrigues <zoldyako@gmail.com>
2026-02-27 13:29:28 -0300

    add: palette.css, base.css, palette preview to storybooks

--- apps/cpdd-site/src/app/layout.tsx
+++ apps/cpdd-site/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import './globals.css';
+import '../styles/globals.css';
 
 import Navbar from '@src/components/common/Navbar';
 import Footer from '@src/components/common/Footer';
