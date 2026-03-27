9116a52560e4f627c6aec8013a543fbf09038ce6
rev-oo <115083816+rev-oo@users.noreply.github.com>
2026-03-27 14:43:35 -0300

    chore: hide faq buttons

--- apps/cpdd-site/src/components/Home/FAQ/FAQ.tsx
+++ apps/cpdd-site/src/components/Home/FAQ/FAQ.tsx
@@ -4,13 +4,14 @@ import { cards } from "./data";
 export default function FAQ() {
     return (
         <section className="full-width layout-grid bg-cpdd-orange-500 text-cpdd-neutral-950">
-            <div className="py-6 grid gap-6 items-center md:grid-cols-[5fr_6fr] md:gap-32">
+            <div className="py-6 grid gap-6 items-center md:grid-cols-[5fr_6fr] md:gap-34">
                 <div>
                     <h2 className="font-heading text-5xl font-semibold mb-2 md:mb-6 md:text-8xl">FAQ</h2>
                     <p className="font-body text-sm md:text-xl">Lorem ipsum dolor sit amet consectetur. Et urna maecenas amet sodales ut tellus enim neque porta. Integer convallis nulla cras convallis. Posuere vulputate tortor ornare vestibulum auctor. Facilisi elementum in rhoncus dictum. Vitae nunc potenti neque pretium semper. Commodo fermentum tellus pulvinar condimentum arcu nunc. Nulla dignissim ultricies amet vitae nibh. Volutpat viverra lacus accumsan sit mi.</p>
                     <button
                         className="button-lg border-4 border-cpdd-neutral-950 rounded-full py-3 text-xl font-bold hidden md:block w-8/10 mx-auto mt-16"
                         type="button"
+                        hidden
                     >
                         Ver Todos
                     </button>
@@ -27,6 +28,7 @@ export default function FAQ() {
                 <button
                     className="button-sm border-2 border-cpdd-neutral-950 rounded-lg py-3 text-sm font-bold md:hidden block w-7/10 mx-auto"
                     type="button"
+                    hidden
                 >
                     Veja mais perguntas
                 </button>
