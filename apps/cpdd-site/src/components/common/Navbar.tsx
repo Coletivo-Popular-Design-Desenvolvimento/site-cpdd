ddae2a33cc12351fba21beeed4eb1a40158a0084
rev-oo <115083816+rev-oo@users.noreply.github.com>
2026-04-03 19:07:08 -0300

    feat: implement  desktop navbar

--- apps/cpdd-site/src/components/common/Navbar.tsx
+++ apps/cpdd-site/src/components/common/Navbar.tsx
@@ -1,7 +1,52 @@
+'use client';
+
+import Link from "next/link"
+import { usePathname } from "next/navigation";
+import CpddLogoTexto from "./CpddLogoTexto";
+
+const menu = [
+    { href: '/projetos', label: 'Projetos' },
+    { href: '/manifesto', label: 'Manifesto' },
+    { href: '/sobre-nos', label: 'Sobre nós' },
+];
+
+const landingPageActiveClassName = [
+    "text-cpdd-orange-500 relative",
+    "before:w-screen before:h-full before:absolute before:right-0 before:top-0 before:bg-cpdd-neutral-950",
+    "after:h-full after:clip-rounded-wedge after:absolute after:left-full after:top-0 after:bg-cpdd-neutral-950",
+].join(' ');
+
+const menuButtonActiveClassName = "rounded-b-4xl bg-cpdd-neutral-950 text-cpdd-orange-500";
+
 export default function Navbar() {
+    const pathname = usePathname();
+
     return (
-        <header>
-            <nav>Navbar placeholder</nav>
+        <header className="bg-cpdd-orange-500 text-cpdd-neutral-950 relative before:w-full before:h-4 before:absolute before:top-0 before:bg-cpdd-neutral-950">
+            <div className="container flex justify-between gap-32">
+                <div className={pathname === "/" ? landingPageActiveClassName : undefined}>
+                    <Link
+                        className="block py-6 isolate"
+                        href="/"
+                    >
+                        <CpddLogoTexto className="fill-current w-80"/>
+                    </Link>
+                </div>
+                <nav>
+                    <ul className="h-full flex gap-6">
+                        {menu.map(({ href, label }) => (
+                            <li key={href}>
+                                <Link
+                                    className={`button-lg flex items-center h-full px-15 ${pathname === href ? menuButtonActiveClassName : ""}`}
+                                    href={href}
+                                >
+                                    {label}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
--- apps/cpdd-site/src/components/common/SvgDefs.tsx
+++ apps/cpdd-site/src/components/common/SvgDefs.tsx
@@ -5,6 +5,9 @@ export default function SvgDefs () {
         <clipPath id="clip-wave" clipPathUnits="objectBoundingBox">
           <path d="M0,0 C0.481,0.1,0.519,1,1,1 H0"></path>
         </clipPath>
+        <clipPath id="clip-rounded-wedge" clipPathUnits="objectBoundingBox">
+          <path d="M0,0 H1 L0.278,0.88 C0.216,0.955,0.112,1,0,1"></path>
+        </clipPath>
       </defs>
     </svg>
   );
