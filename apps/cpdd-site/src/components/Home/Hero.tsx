b3f8b6ac094b35a845daf8dda3e5f1ae5bc67c66
José Victor Dias Rodrigues <zoldyako@gmail.com>
2026-04-10 19:04:25 -0300

    add: hero component

--- apps/cpdd-site/src/components/Home/Hero.tsx
+++ apps/cpdd-site/src/components/Home/Hero.tsx
@@ -1,18 +1,18 @@
 export default function Hero() {
     return (
-        <div className="h-200 flex items-center px-12 bg-cpdd-orange-500 text-black">
-            <div className="w-sm flex flex-col gap-11">
-                <h1 className="heading-lg">Lorem Ipsum</h1>
-                <p className="body-sm">
+        <div className="full-width layout-grid h-200 flex items-center bg-cpdd-orange-500 text-black">
+            <div className="max-w-108 flex flex-col gap-11">
+                <h1 className="display-lg">Lorem Ipsum</h1>
+                <p className="body-lg">
                     Lorem ipsum dolor sit amet consectetur. Amet blandit leo imperdiet tristique
                     scelerisque turpis enim lorem id. Dui dui eu accumsan sit iaculis pretium morbi.
                     Velit volutpat dignissim ullamcorper amet eu at magna nec non.
                 </p>
                 <div className="flex flex-col items-start gap-4">
-                    <button className="w-xs button-sm px-12 py-1 border-2 rounded-full border-black">
+                    <button className="w-full button-lg px-12 py-1 border-2 rounded-full border-black">
                         APOIA.se
                     </button>
-                    <button className="w-xs button-sm px-12 py-1 border-2 rounded-full border-black">
+                    <button className="w-full button-lg px-12 py-1 border-2 rounded-full border-black">
                         JUNTE-SE À NÓS
                     </button>
                 </div>
