ac644557b9a81d7691dd627c5ce81e524ac93eaf
José Victor Dias Rodrigues <zoldyako@gmail.com>
2026-04-13 15:43:46 -0300

    fix: spacing and font

--- apps/cpdd-site/src/components/Home/Hero.tsx
+++ apps/cpdd-site/src/components/Home/Hero.tsx
@@ -1,14 +1,14 @@
 export default function Hero() {
     return (
-        <div className="full-width layout-grid h-200 flex items-center bg-cpdd-orange-500 text-black">
-            <div className="sm:max-w-108 flex flex-col gap-11">
-                <h1 className="display-md text-end sm:text-start sm:display-lg">Venha fazer parte da revolução brasileira</h1>
-                <p className="body-lg">
+        <div className="full-width layout-grid py-8 sm:py-36 flex items-center bg-cpdd-orange-500 text-black">
+            <div className="flex flex-col gap-8 sm:gap-12">
+                <h1 className="heading-lg text-end sm:text-start sm:display-lg">Venha fazer parte da revolução brasileira</h1>
+                <p className="sm:max-w-108 text-end sm:text-start body-lg">
                     Lorem ipsum dolor sit amet consectetur. Amet blandit leo imperdiet tristique
                     scelerisque turpis enim lorem id. Dui dui eu accumsan sit iaculis pretium morbi.
                     Velit volutpat dignissim ullamcorper amet eu at magna nec non.
                 </p>
-                <div className="flex flex-col items-start gap-4">
+                <div className="sm:max-w-108 flex flex-col items-start gap-4">
                     <button className="w-full button-lg px-12 py-1 border-2 rounded-full border-black">
                         APOIA.se
                     </button>
