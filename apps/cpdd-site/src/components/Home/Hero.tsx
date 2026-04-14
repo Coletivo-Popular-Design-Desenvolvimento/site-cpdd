7fafec2df378193a78aba0a689c0b8b2867f15bf
José Victor Dias Rodrigues <zoldyako@gmail.com>
2026-04-14 19:48:57 -0300

    fix: use <a> instead of <button> and hide apoia.se button

--- apps/cpdd-site/src/components/Home/Hero.tsx
+++ apps/cpdd-site/src/components/Home/Hero.tsx
@@ -1,6 +1,8 @@
+import ApoiaSeLogoTexto from '../common/ApoiaseLogoTexto';
+
 export default function Hero() {
     return (
-        <section className="bg-cpdd-orange-500 text-black">
+        <section className="bg-cpdd-orange-500 text-cpdd-neutral-950">
             <div className="container py-8 sm:py-36 flex items-center">
                 <div className="flex flex-col gap-8 sm:gap-12">
                     <h1 className="heading-lg text-end sm:text-start sm:display-lg">
@@ -12,12 +14,20 @@ export default function Hero() {
                         morbi. Velit volutpat dignissim ullamcorper amet eu at magna nec non.
                     </p>
                     <div className="sm:max-w-108 flex flex-col items-start gap-4">
-                        <button className="w-full button-lg px-12 py-1 border-2 rounded-full border-black">
-                            APOIA.se
-                        </button>
-                        <button className="w-full button-lg px-12 py-1 border-2 rounded-full border-black">
-                            JUNTE-SE À NÓS
-                        </button>
+                        <a
+                            href="#"
+                            target="_blank"
+                            className="hidden w-full h-10 button-lg px-12 py-1 border-2 rounded-full border-black transition-colors hover:bg-cpdd-neutral-950 hover:text-cpdd-orange-500"
+                        >
+                            <p className="w-full text-center">APOIA.se</p> {/*TO-DO: Adicionar logo do apoia.se}*/}
+                        </a>
+                        <a
+                            href="#"
+                            target="_blank"
+                            className="w-full h-10 button-lg px-12 py-1 border-2 rounded-full border-black transition-colors hover:bg-cpdd-neutral-950 hover:text-cpdd-orange-500"
+                        >
+                            <p className="w-full text-center">JUNTE-SE À NÓS</p>
+                        </a>
                     </div>
                 </div>
             </div>
