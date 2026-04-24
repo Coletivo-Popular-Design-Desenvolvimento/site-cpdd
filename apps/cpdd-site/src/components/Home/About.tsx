5c778224f658a1bbf7f2e96f15bb9495bd05a7ec
rev-oo <115083816+rev-oo@users.noreply.github.com>
2026-04-24 15:03:28 -0300

    feat: implement about section

--- apps/cpdd-site/src/components/Home/About.tsx
+++ apps/cpdd-site/src/components/Home/About.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
+
 export default function About() {
     return (
-        <div>
-            <p>About placeholder</p>
-        </div>
+        <section className="bg-cpdd-neutral-950 text-cpdd-neutral-50">
+            <div className="container min-h-12 md:min-h-25 flex items-center">
+                <h2 className="heading-sm md:display-md text-cpdd-orange-500">Sobre nós</h2>
+            </div>
+            <div className="relative body-md lg:body-lg">
+                <div className="container relative aspect-video md:aspect-auto md:h-250">
+                    <Image
+                        alt=""
+                        src="https://picsum.photos/seed/about/1920/1080"
+                        fill={true}
+                        className="object-cover md:clip-about-image"
+                    />
+                </div>
+                <div className="container py-6 grid gap-4 md:grid-cols-2 md:grid-rows-2 md:items-center md:gap-24 md:absolute md:inset-0">
+                    <p className="md:col-2">
+                        Lorem ipsum dolor sit amet consectetur. Sed scelerisque dapibus pellentesque luctus nec faucibus sed. Nunc mauris mi et tincidunt est. Mauris sed tellus vestibulum pellentesque massa aliquam libero augue purus. Mi placerat volutpat fermentum in feugiat eget. Sodales senectus vitae rhoncus vitae dui aliquet phasellus sit. Quisque feugiat urna mollis ipsum convallis viverra. Lorem ipsum dolor sit amet consectetur. Sed scelerisque dapibus pellentesque luctus nec faucibus sed. Nunc mauris mi et tincidunt est. Mauris sed tellus vestibulum pellentesque massa aliquam libero augue purus. Mi placerat volutpat fermentum in feugiat eget. Sodales senectus vitae rhoncus vitae dui aliquet phasellus sit. 
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur. Sed scelerisque dapibus pellentesque luctus nec faucibus sed. Nunc mauris mi et tincidunt est. Mauris sed tellus vestibulum pellentesque massa aliquam libero augue purus. Mi placerat volutpat fermentum in feugiat eget. Sodales senectus vitae rhoncus vitae dui aliquet phasellus sit. Quisque feugiat urna mollis ipsum convallis viverra. Lorem ipsum dolor sit amet consectetur. Sed scelerisque dapibus pellentesque luctus nec faucibus sed.
+                    </p>
+                </div>
+            </div>
+        </section>
     );
 }
--- apps/cpdd-site/src/components/common/SvgDefs.tsx
+++ apps/cpdd-site/src/components/common/SvgDefs.tsx
@@ -8,6 +8,9 @@ export default function SvgDefs () {
         <clipPath id="clip-rounded-wedge" clipPathUnits="objectBoundingBox">
           <path d="M0,0 H1 L0.278,0.88 C0.216,0.955,0.112,1,0,1"></path>
         </clipPath>
+        <clipPath id="clip-about-image" clipPathUnits="objectBoundingBox">
+          <path d="M0.411,0 C0.46,0,0.5,0.057,0.5,0.128 V0.351 C0.504,0.434,0.552,0.5,0.611,0.5 H0.911 C0.96,0.5,1,0.557,1,0.628 V1 H0.589 C0.54,1,0.5,0.943,0.5,0.872 V0.649 C0.496,0.566,0.448,0.5,0.389,0.5 H0.089 C0.04,0.5,0,0.443,0,0.372 V0"></path>
+        </clipPath>
       </defs>
     </svg>
   );
