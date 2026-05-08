1e2a1e6facda133080cce43b742284a8b289fdf2
rev-oo <115083816+rev-oo@users.noreply.github.com>
2026-05-08 14:51:15 -0300

    Update Manifest.tsx

--- apps/cpdd-site/src/components/Home/Manifest.tsx
+++ apps/cpdd-site/src/components/Home/Manifest.tsx
@@ -8,7 +8,7 @@ export default function Manifest() {
         <section className="bg-cpdd-neutral-50 text-cpdd-neutral-950">
             <div className="container py-8 md:py-16">
                 <div className="md:px-27">
-                    <h2 className="heading-md mb-8 md:display-md md:mb-11">Manifesto</h2>
+                    <h2 className="heading-sm mb-8 md:display-md md:mb-11">Manifesto</h2>
                     <div className="space-y-4 body-sm text-sm md:body-lg">
                         {paragraphs.map((text, i) => <p key={i}>{text}</p>)}
                     </div>
