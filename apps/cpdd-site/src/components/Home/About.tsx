c9b142cb8109dcaf85cda86e95fae24a14e4ef43
José Victor Dias Rodrigues <zoldyako@gmail.com>
2026-05-15 11:49:39 -0300

    Add: textos, imagem de fuldo e alt text

--- apps/cpdd-site/src/components/Home/About.tsx
+++ apps/cpdd-site/src/components/Home/About.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
-import WaveDivider from "../common/WaveDivider";
+import Image from 'next/image';
+import WaveDivider from '../common/WaveDivider';
 
-const imageSrc = "https://picsum.photos/seed/about/1920/1080";
-const text1 = "Lorem ipsum dolor sit amet consectetur. Sed scelerisque dapibus pellentesque luctus nec faucibus sed. Nunc mauris mi et tincidunt est. Mauris sed tellus vestibulum pellentesque massa aliquam libero augue purus. Mi placerat volutpat fermentum in feugiat eget. Sodales senectus vitae rhoncus vitae dui aliquet phasellus sit. Quisque feugiat urna mollis ipsum convallis viverra. Lorem ipsum dolor sit amet consectetur. Sed scelerisque dapibus pellentesque luctus nec faucibus sed. Nunc mauris mi et tincidunt est. Mauris sed tellus vestibulum pellentesque massa aliquam libero augue purus. Mi placerat volutpat fermentum in feugiat eget. Sodales senectus vitae rhoncus vitae dui aliquet phasellus sit.";
-const text2 = "Lorem ipsum dolor sit amet consectetur. Sed scelerisque dapibus pellentesque luctus nec faucibus sed. Nunc mauris mi et tincidunt est. Mauris sed tellus vestibulum pellentesque massa aliquam libero augue purus. Mi placerat volutpat fermentum in feugiat eget. Sodales senectus vitae rhoncus vitae dui aliquet phasellus sit. Quisque feugiat urna mollis ipsum convallis viverra. Lorem ipsum dolor sit amet consectetur. Sed scelerisque dapibus pellentesque luctus nec faucibus sed.";
+const imageSrc = 'leninSpeech.webp';
+const text1 =
+    'O CPDD é um coletivo independente de trabalhadores de tecnologia, design, comunicação e gestão que usam seu talento para transformar a sociedade. Se quem cria conhecimento não decide seu uso, a tecnologia vira arma das grandes empresas, não da maioria. Atuamos para construir um novo mundo, onde a soberania digital seja uma realidade e a tecnologia esteja nas mãos da classe trabalhadora.';
+const text2 =
+    'Sozinhos somos peças fáceis de trocar. Juntos viramos força que muda as regras do jogo. Organizar-se faz crescer a consciência crítica, cria redes de apoio e protege a categoria das pressões do mercado. Queremos quebrar a dependência das Big Techs e lembrar que produção digital também faz parte da luta de classes.';
 
 export default function About() {
     return (
@@ -23,19 +25,15 @@ export default function About() {
             <div className="relative body-md lg:body-lg">
                 <div className="container relative aspect-video md:aspect-auto md:h-250">
                     <Image
-                        alt=""
+                        alt='A imagem de fundo é uma pintura retratando Lenin discursando para uma multidão de operários e soldados. A pintura é titulada "Lenin Proclama Poder Soviético", pintada por Vladimir Serov'
                         src={imageSrc}
                         fill={true}
                         className="object-cover md:clip-about-image"
                     />
                 </div>
                 <div className="container py-6 grid gap-4 md:grid-cols-2 md:grid-rows-2 md:items-center md:gap-24 md:absolute md:inset-0">
-                    <p className="md:col-2">
-                        {text1}
-                    </p>
-                    <p>
-                        {text2}
-                    </p>
+                    <p className="md:col-2">{text1}</p>
+                    <p>{text2}</p>
                 </div>
             </div>
             <WaveDivider
